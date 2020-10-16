1c643f5e2da1f065f2b395de872dab1ff9169731
unknown <felipelambiazzi17@gmail.com>
2020-10-16 11:59:10 -0300

    subindo as libs

---
+++ Documents/NetBeansProjects/Rick_and_Morty_Shopping/src/rick_and_morty_shopping/Rick_and_Morty_Shopping.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rick_and_morty_shopping;
+
+/**
+ *
+ * @author Win10
+ */
+public class Rick_and_Morty_Shopping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
